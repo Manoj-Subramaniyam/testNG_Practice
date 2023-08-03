cc76851b208d33bb6d123dbea0a1c9978bc5522e
Manoj-Subramaniyam <manojsubramaniyam96@gmail.com>
2023-08-03 21:36:58 +0530

    testNG file1

---
+++ TestCase_1_Window.java
@@ -0,0 +1,52 @@
+package testNG;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+public class TestCase_1_Window {
+	@Test
+	public void windowHandles() throws InterruptedException {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.get("https://www.leafground.com");
+		driver.findElement(By.xpath("//span[text()='Browser']/ancestor::a")).click();
+		driver.findElement(By.xpath("//span[text()='Window']")).click();
+		String currentWindow = driver.getWindowHandle();
+		driver.findElement(By.xpath("//h5[text()='Click and Confirm new Window Opens']/following-sibling::button"))
+				.click();
+		Set<String> numOfWindows = driver.getWindowHandles();
+		List<String> numOfWindowsList = new ArrayList<String>(numOfWindows);
+		// System.out.println(numOfWindowsList.get(1));
+		driver.switchTo().window(numOfWindowsList.get(1));
+		driver.close();
+		driver.switchTo().window(currentWindow);
+		driver.findElement(By.xpath("//h5[text()='Close all windows except Primary']/following-sibling::button"))
+				.click();
+		WebDriverWait wait= new WebDriverWait(driver,Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.numberOfWindowsToBe(4));
+		numOfWindows = driver.getWindowHandles();
+		numOfWindowsList = new ArrayList<String>(numOfWindows);
+		
+		for (String window : numOfWindowsList) {
+			if (!window.equals(currentWindow)) {
+				driver.switchTo().window(window);
+					driver.close();
+				
+			}
+			
+		}
+		driver.switchTo().window(currentWindow);
+		driver.close();
+	}
+}
