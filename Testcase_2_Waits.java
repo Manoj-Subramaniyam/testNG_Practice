b3535148add9b2276ff2ee0334cff4d58842b218
Manoj-Subramaniyam <manojsubramaniyam96@gmail.com>
2023-08-06 09:14:09 +0530

    updated waits file

--- Testcase_2_Waits.java
+++ Testcase_2_Waits.java
@@ -13,24 +13,21 @@ import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class Testcase_2_Waits {
+public class Testcase_2_Waits extends ProjectSpecificClass {
+	
 	@Test
 	public void waits() {
-		WebDriverManager.edgedriver().setup();
-		WebDriver driver = new EdgeDriver();
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		driver.get("https://www.leafground.com/");
+		
 		driver.findElement(By.xpath("//span[text()='Element']/ancestor::a")).click();
 		driver.findElement(By.xpath("//span[text()='Waits']")).click();
 		// Visibility of element
 		driver.findElement(By.xpath("//h5[contains(text(),'Wait for Visibility')]/following::button[1]")).click();
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-		WebElement ele = driver.findElement(By.xpath("//*[contains(text(),'I am here')]"));
-		wait.until(ExpectedConditions.visibilityOf(ele));
+		
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[contains(text(),'I am here')]")));
 
-		if (ele.isDisplayed())
+		if (driver.findElement(By.xpath("//*[contains(text(),'I am here')]")).isDisplayed())
 			System.out.println("element is Visible");
-		driver.quit();
+		
 	}
 }
