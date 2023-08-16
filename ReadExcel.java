026ce1abdf7899fcc2ea33519b6d3743603544a2
Manoj-Subramaniyam <manojsubramaniyam96@gmail.com>
2023-08-16 21:57:25 +0530

    updated Read Excel

--- ReadExcel.java
+++ ReadExcel.java
@@ -2,6 +2,8 @@ package week5;
 
 import java.io.IOException;
 
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.testng.annotations.Test;
@@ -10,11 +12,27 @@ public class ReadExcel {
 	@Test
 	public void getReadExcel() throws IOException
 	{
-		XSSFWorkbook book= new XSSFWorkbook("path");
+		XSSFWorkbook book= new XSSFWorkbook("C:/Users/manoj.subramaniyam/Downloads/testdata.xlsx");
 		XSSFSheet sheet= book.getSheetAt(0);
 		int row=sheet.getLastRowNum();
 		short columnCount=sheet.getRow(0).getLastCellNum();
 		System.out.println("Number of rows in the Excel:"+row);
 		System.out.println("Number of columns in the Excel:"+columnCount);
+		
+		XSSFRow rowNum= sheet.getRow(1);
+		XSSFCell column=rowNum.getCell(1);
+		System.out.println(column.getNumericCellValue());
+		
+		for(int i=1;i<=row;i++)
+		{
+			rowNum= sheet.getRow(i);
+			for(int j=0;j<columnCount;j++)
+			{
+				column=rowNum.getCell(j);
+				System.out.println(column.getNumericCellValue());
+			}
+		}
+		
+		
 	}
 }
